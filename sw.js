90df7fea177680e152f8f92c9a34dbb326b89e71
IamWaishing <waishing2702@gmail.com>
2021-02-20 06:15:19 +0800

    Change to PWA

--- sw.js
+++ sw.js
@@ -24,6 +24,13 @@ self.addEventListener('install', e => {
 
 self.addEventListener('fetch', (event) => {
 	event.respondWith(
-	  caches.match(event.request)
+	  caches.match(event.request).then((resp) => {
+		return resp || fetch(event.request).then((response) => {
+		  return caches.open('v1').then((cache) => {
+			cache.put(event.request, response.clone());
+			return response;
+		  });
+		});
+	  })
 	);
 });

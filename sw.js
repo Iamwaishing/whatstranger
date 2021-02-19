a9d0f26e1bc97bf52131a1c438e3b357e154f29f
IamWaishing <waishing2702@gmail.com>
2021-02-19 15:12:31 +0800

    Change to PWA

--- sw.js
+++ sw.js
@@ -1,25 +1,26 @@
 // install
 self.addEventListener('install', e => {
-	e.waitUntil(
-		caches.open("static").then(cache => {
-			return cache.addAll(["./",
-							"./index.js",
-							"./css/default.css",
-							"./images/whatsapp.png",
-							"./images/icon-72x72.png",
-							"./images/icon-96x96.png",
-							"./images/icon-128x128.png",
-							"./images/icon-144x144.png",
-							"./images/icon-152x152.png",
-							"./images/icon-192x192.png",
-							"./images/icon-384x384.png",
-							"./images/icon-512x512.png"
-							]);
-		})
-	);
+	console.log("Install!");
+//	e.waitUntil(
+//		caches.open("static").then(cache => {
+//			return cache.addAll(["./",
+//							"./index.js",
+//							"./css/default.css",
+//							"./images/whatsapp.png",
+//							"./images/icon-72x72.png",
+//							"./images/icon-96x96.png",
+//							"./images/icon-128x128.png",
+//							"./images/icon-144x144.png",
+//							"./images/icon-152x152.png",
+//							"./images/icon-192x192.png",
+//							"./images/icon-384x384.png",
+//							"./images/icon-512x512.png"
+//							]);
+//		})
+//	);
 });
 
 
-self.addEventListener("fetch", e => {
-	console.log(`inter for: ${e.request.url}`);
-});
+//self.addEventListener("fetch", e => {
+//	console.log(`inter for: ${e.request.url}`);
+//});
